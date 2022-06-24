32f7e031e28de20eb269b1a86330588e030ca889
Emanuel Guevara <92771229+thesupermanny@users.noreply.github.com>
2022-06-24 00:31:25 -0400

    added Listing Details 2nd commit

---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+import AppText from '../components/AppText';
+import colors from '../config/colors';
+import ListItem from '../components/ListItem';
+
+function ListingDetailsScreen(props) {
+  return (
+    <View>
+      <Image style={styles.image} source={require('../assets/boatSale.jpg')} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Boat For sale</AppText>
+        <AppText style={styles.price}>$1,000</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require('../assets/headShotTwo.jpg')}
+            title='Emanuel Guevara'
+            subTitle='12 Listings'
+          />
+        </View>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '700',
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  userContainer: {
+    marginVertical: 30,
+  },
+});
+
+export default ListingDetailsScreen;
