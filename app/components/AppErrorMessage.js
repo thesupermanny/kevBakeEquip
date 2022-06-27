8f6cac5b0348c57eda4608de68ee2527ce1cab8b
Emanuel Guevara <92771229+thesupermanny@users.noreply.github.com>
2022-06-26 23:36:48 -0400

    log in touched state for errors

---
+++ app/components/AppErrorMessage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import colors from '../config/colors';
+
+import AppText from './AppText';
+
+function AppErrorMessage({ error, visible }) {
+  if (!visible || !error) return null;
+
+  return <AppText style={styles.text}>{error}</AppText>;
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: colors.error,
+  },
+});
+export default AppErrorMessage;
--- app/screens/LogInScreen.js
+++ app/screens/LogInScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Image } from 'react-native';
 import { Formik } from 'formik';
-
-import AppButton from '../components/AppButton';
-import AppTextInput from '../components/AppTextInput';
 import * as Yup from 'yup';
 
-import Screen from '../components/Screen';
+import AppButton from '../components/AppButton';
 import AppText from '../components/AppText';
+import AppTextInput from '../components/AppTextInput';
+import Screen from '../components/Screen';
+import AppErrorMessage from '../components/AppErrorMessage';
 
 const validationShema = Yup.object().shape({
   email: Yup.string().required().email().label('Email'),
@@ -27,29 +27,34 @@ function LogInScreen(props) {
         onSubmit={(values) => console.log(values)}
         validationSchema={validationShema}
       >
-        {({ handleChange, handleSubmit, errors }) => (
+        {({ handleChange, handleSubmit, errors, setTouched, touched }) => (
           <>
             <AppTextInput
               autoCapitalize='none'
               autoCorrect={false}
               icon='email'
               keyboardType='email-address'
+              onBlur={() => setTouched('email')}
               onChangeText={handleChange('email')}
               placeholder='email'
               //Allows IOS to auto fill from keychain
-              textContentType='email'
+              textContentType='emailAddress'
             />
-            <AppText style={{ color: 'red' }}>{errors.email}</AppText>
+            <AppErrorMessage error={errors.email} visible={touched.email} />
             <AppTextInput
               autoCapitalize='none'
               autoCorrect={false}
               icon='lock'
+              onBlur={() => setTouched('password')}
               onChangeText={handleChange('password')}
               placeholder='Password'
               secureTextEntry={true}
               textContentType='password'
             />
-            <AppText style={{ color: 'red' }}>{errors.password}</AppText>
+            <AppErrorMessage
+              error={errors.password}
+              visible={touched.password}
+            />
             <AppButton color='error' title='Log In' onPress={handleSubmit} />
           </>
         )}
