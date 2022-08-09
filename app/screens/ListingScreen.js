49e21935484f4c31971e2920170b3bec46239f06
Emanuel Guevara <92771229+thesupermanny@users.noreply.github.com>
2022-08-08 22:11:22 -0400

    working on fetcingData

--- app/screens/ListingScreen.js
+++ app/screens/ListingScreen.js
@@ -1,33 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 
+import listingsApi from '../api/listings';
 import Card from '../components/Card';
 import colors from '../config/colors';
 import routes from '../navigation/routes';
 import Screen from '../components/Screen';
+import AppText from '../components/AppText';
 
-const listings = [
-  {
-    id: 1,
-    title: 'Leaf for sale',
-    price: 10.0,
-    image: require('../assets/listingImages/leaf.jpg'),
-  },
-  {
-    id: 2,
-    title: 'Statue for sale',
-    price: 4998.98,
-    image: require('../assets/listingImages/statue.jpg'),
-  },
-  {
-    id: 3,
-    title: 'Building for sale',
-    price: 599.99,
-    image: require('../assets/listingImages/building.jpg'),
-  },
-];
+// const listings = [
+//   {
+//     id: 1,
+//     title: 'Leaf for sale',
+//     price: 10.0,
+//     image: require('../assets/listingImages/leaf.jpg'),
+//   },
+//   {
+//     id: 2,
+//     title: 'Statue for sale',
+//     price: 4998.98,
+//     image: require('../assets/listingImages/statue.jpg'),
+//   },
+//   {
+//     id: 3,
+//     title: 'Building for sale',
+//     price: 599.99,
+//     image: require('../assets/listingImages/building.jpg'),
+//   },
+// ];
 
 function ListingScreen({ navigation }) {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  const loadListings = async () => {
+    const response = await listingsApi.getListings();
+    setListings(response.data);
+  };
+
   return (
     <Screen style={styles.screen}>
       <FlatList
@@ -37,7 +50,7 @@ function ListingScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}
@@ -52,4 +65,5 @@ const styles = StyleSheet.create({
     backgroundColor: colors.light,
   },
 });
+
 export default ListingScreen;
